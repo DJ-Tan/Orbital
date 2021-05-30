961271f04cc919b97a7dba624b4d7f01a55ddd63
Daren Tan <daren9901@gmail.com>
2021-05-30 22:54:21 +0800

    v1.0

--- app/src/main/java/com/example/loginpage/ProfileFragment.java
+++ app/src/main/java/com/example/loginpage/ProfileFragment.java
@@ -12,13 +12,18 @@ import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        int userid = getArguments().getInt("userid");
-//        Log.d("orbital", String.valueOf(userid));
+    int userid;
 
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            userid = bundle.getInt("userid");
+        }
+        Log.d("orbital", String.valueOf(userid));
+
+        return view;
     }
 
 }
